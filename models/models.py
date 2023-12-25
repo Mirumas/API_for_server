1ec16e84c40c696d5972a311f2d52acf424cf849
iliab <iliabob67@gmail.com>
2023-12-25 13:10:07 +0700

    good

--- models/models.py
+++ models/models.py
@@ -6,11 +6,11 @@ class User(BaseModel):
     FirstName: Union[str, None] = None
     LastName: Union[str, None] = None
     Nickname: Union[str, None] = None
-    id: Annotated[Union[int, None], Field(default=100, ge=1, lt=200)] = None
+    user_id: Annotated[Union[int, None], Field(default=100, ge=1, lt=200)] = None
 
 
 class UserDB(User):
-    password: Annotated[Union[str, None], Field(max_length=200, min_length=5)] = None
+    password: Annotated[Union[str, None], Field(max_length=50, min_length=5)] = None
 
 
 class Response(BaseModel):
--- public/users_methods.py
+++ public/users_methods.py
@@ -1,25 +1,25 @@
-import uuid
-from fastapi.responses import JSONResponse, FileResponse
-import hashlib
 from fastapi import APIRouter, Body
 from models.models import User, UserDB, Response
 from typing import Union, Annotated
+import random
+import string
 
 users_router = APIRouter()
 
-users_database = [UserDB(FirstName='Ilya', LastName='Bob', Nickname='Shama', id=1, password='qwerty'),
-                  UserDB(FirstName='Maksim', LastName='Sob', Nickname='Aka', id=2, password='qazwsx'),
-                  UserDB(FirstName='Bill', LastName='De', Nickname='Baron', id=3, password='123456'),
-                  UserDB(FirstName='Mikhail ', LastName='Vladimirovich ', Nickname='Krug', id=4, password='222001')]
+users_database = [UserDB(FirstName='Ilya', LastName='Bob', Nickname='Shama', user_id=1, password='qwerty'),
+                  UserDB(FirstName='Maksim', LastName='Sob', Nickname='Aka', user_id=2, password='qazwsx'),
+                  UserDB(FirstName='Bill', LastName='De', Nickname='Baron', user_id=3, password='123456'),
+                  UserDB(FirstName='Mikhail ', LastName='Vladimirovich ', Nickname='Krug', user_id=4, password='222001')]
 
 
-def coder_password(cod: str):
-    result = cod * 2
+def generate_password():
+    result = ''.join(random.choice(string.ascii_uppercase) for _ in range(random.randint(5, 50)))
+    return result
 
 
 def find_user(user_id: int) -> Union[UserDB, None]:
     for user in users_database:
-        if user.id == user_id:
+        if user.user_id == user_id:
             return user
     return None
 
@@ -29,9 +29,9 @@ def get_users():
     return users_database
 
 
-@users_router.get("/api/users/{id}", response_model=Union[User, Response])
-def get_user(id: int):
-    user = find_user(id)
+@users_router.get("/api/users/{user_id}", response_model=Union[User, Response])
+def get_user(user_id: int):
+    user = find_user(user_id)
     if user is None:
         return Response(message="Пользователь не найден")
     else:
@@ -41,25 +41,27 @@ def get_user(id: int):
 
 @users_router.post("/api/users", response_model=Union[User, Response])
 def create_user(item: Annotated[User, Body(embed=True, description="Создание нового пользователя")]):
-    user = UserDB(name=item.name, id=item.id, password=coder_password(item.name))
+    user = UserDB(FirstName=item.FirstName, LastName=item.LastName, Nickname=item.Nickname, user_id=item.user_id, password=generate_password())
     users_database.append(user)
     return user
 
 
 @users_router.put("/api/users", response_model=Union[User, Response])
 def edit_user(item: Annotated[User, Body(embed=True, description="Редактирование пользователя")]):
-    user = find_user(item.id)
+    user = find_user(item.user_id)
     if user is None:
         return Response(message="Пользователь не найден")
     else:
-        user.id = item.id
-        user.name = item.name
+        user.user_id = item.user_id
+        user.FirstName = item.FirstName
+        user.LastName = item.LastName
+        user.Nickname = item.Nickname
         return user
 
 
-@users_router.delete("/api/users/{id}", response_model=Union[list[User], None])
-def delete_user(id: int):
-    user = find_user(id)
+@users_router.delete("/api/users/{user_id}", response_model=Union[list[User], None])
+def delete_user(user_id: int):
+    user = find_user(user_id)
     if user is None:
         return Response(message="Пользователь не найден")
     else:
